8d5e6932b9f11dda4d4eb59c4013a439a4a26c2b
Appa <appa247@proton.me>
2024-04-27 13:06:16 +0200

    feat: migrate project to angular 17 folder structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [HeaderComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'portfolio';
